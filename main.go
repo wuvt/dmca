029e25e5adccdae42e7700cb7019df6025ebacce
mutantmonkey <mutantmonkey@mutantmonkey.in>
2017-10-09 18:44:50 -0700

    Use metaflac to retag files

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"os"
+	"os/exec"
 	"regexp"
+	"strings"
+	"time"
 
-	"github.com/eaburns/bit"
 	"golang.org/x/net/publicsuffix"
 )
 
@@ -52,35 +55,6 @@ type TrackJSON struct {
 	Title          string
 	Track_MBID     string
 	Track_Num      uint64
-}
-
-type BlockHeader struct {
-	LastBlock bool
-	Type      uint8
-	Length    int64
-}
-
-func parseBlockHeader(r io.Reader) (block *BlockHeader, err error) {
-	br := bit.NewReader(&io.LimitedReader{R: r, N: 32})
-	fields, err := br.ReadFields(1, 7, 24)
-	if err != nil {
-		return
-	}
-
-	return &BlockHeader{
-		LastBlock: fields[0] == 1,
-		Type:      uint8(fields[1]),
-		Length:    int64(fields[2]),
-	}, nil
-}
-
-func (b *BlockHeader) Marshal() []byte {
-	packed := []byte{b.Type, byte(b.Length >> 16), byte(b.Length >> 8), byte(b.Length)}
-	if b.LastBlock {
-		// set the last block flag bit to 1
-		packed[0] ^= 0x80
-	}
-	return packed
 }
 
 func loadTrackInfo(trackID string) (data *TrackJSON, err error) {
@@ -138,98 +112,61 @@ func trackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data, err := loadTrackInfo(match[1])
+	track, err := loadTrackInfo(match[1])
 	if _, ok := err.(TrackNotFoundError); ok {
 		http.NotFound(w, r)
 		return
 	} else if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		log.Printf("Unable to load track information: %v\n", err)
+		log.Printf("Failed to load track information: %v\n", err)
 		return
 	}
 
-	resp, err := http.Get(fmt.Sprintf("%s/%s/music/%s", config.mossURL, url.PathEscape(data.Holding_ID), url.PathEscape(data.File_Path)))
+	resp, err := http.Get(fmt.Sprintf("%s/%s/music/%s", config.mossURL, url.PathEscape(track.Holding_ID), url.PathEscape(track.File_Path)))
 	if err != nil || resp.StatusCode != 200 {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		log.Printf("Unable to access track: %v\n", err)
+		log.Printf("Failed to access track: %v\n", err)
+		return
 	}
 	defer resp.Body.Close()
 
-	magic := []byte("fLaC")
-
-	// check that the file we're reading starts with fLaC
-	var fileMagic [4]byte
-	_, err = io.ReadFull(resp.Body, fileMagic[:])
-	if err != nil || !bytes.Equal(fileMagic[:], magic) {
+	tmpf, err := ioutil.TempFile("", "dmca")
+	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		log.Printf("Unable to decode track: %v\n", err)
+		log.Printf("Failed to create temporary file: %v\n", err)
+		return
+	}
+	defer os.Remove(tmpf.Name())
+
+	if _, err := io.Copy(tmpf, resp.Body); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Failed to download track: %v\n", err)
+		return
+	}
+	if err := tmpf.Close(); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Failed to close temporary file: %v\n", err)
 		return
 	}
 
-	// now that we've done the preliminary check, we can start writing a
-	// response
-	w.Header().Add("Content-Type", "audio/flac")
-	if _, err = w.Write(magic); err != nil {
-		log.Printf("Failed to write response: %v\n", err)
-	}
-
-	// copy the STREAMINFO block; per the FLAC specification, the metadata
-	// block header is 0x4 bytes long and the STREAMINFO block itself is
-	// 0x22 bytes long
-	if _, err := io.CopyN(w, resp.Body, 0x26); err != nil {
-		log.Printf("Unable to decode track: %v\n", err)
+	cmd := exec.Command("metaflac", "--remove-tag=ARTIST", "--remove-tag=TITLE", "--remove-tag=ALBUM", "--remove-tag=LABEL", "--import-tags-from=-", tmpf.Name())
+	// FIXME: actually use the data from impala here for the album and label
+	cmd.Stdin = strings.NewReader(fmt.Sprintf("ARTIST=%s\nTITLE=%s\nALBUM=dmca test\nLABEL=dmca test\n", track.Artist, track.Title))
+	if err := cmd.Run(); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Failed to modify track metadata: %v\n", err)
 		return
 	}
 
-	for {
-		block, err := parseBlockHeader(resp.Body)
-		if err != nil {
-			log.Printf("Failed to parse block header: %v\n", err)
-			return
-		}
-
-		// FIXME: remove this debugging code
-		log.Printf("Block: %v, %d, %d\n", block.LastBlock, block.Type, block.Length)
-
-		if _, err := w.Write(block.Marshal()); err != nil {
-			log.Printf("Failed to write response: %v\n", err)
-			return
-		}
-
-		if _, err := io.CopyN(w, resp.Body, block.Length); err != nil {
-			log.Printf("Unable to decode track: %v\n", err)
-			return
-		}
-
-		if block.LastBlock {
-			break
-		}
-	}
-
-	// TODO: walk through each METADATA block. we know when we're at the
-	// end when the last metadata block flag is set to "1"
-	// look for VORBIS_COMMENT block (type 4)
-	// then look to see if a PADDING block (type 1) that follows it
-	// if no VORBIS_COMMENT block is found or there is no PADDING block of
-	// sufficient (how big?) size, then more extensive metadata rewriting
-	// logic will be necessary
-
-	// the metadata block header is 0x4 bytes long and the last field
-	// contains the length of the rest of the block
-	// make a struct for the metadata block header
-	// for the vorbis comment block, there's a vendor field and then a tags
-	// field that is just a list of all the vorbis comment structs
-
-	// if we've reached the last metadata block and haven't found the
-	// vorbis comments metadata block, add one now (and consider adding
-	// padding too). remember to set the last block flag appropriately
-
-	// finally, return the rest of the file, including the actual audio
-	// frame data
-	if _, err := io.Copy(w, resp.Body); err != nil {
-		log.Printf("Unable to decode track: %v\n", err)
+	f, err := os.Open(tmpf.Name())
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Failed to load track: %v\n", err)
 		return
 	}
+	defer f.Close()
+
+	http.ServeContent(w, r, fmt.Sprintf("%s.flac", track.ID), time.Now(), f)
 }
 
 func main() {
